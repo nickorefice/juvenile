7f0f8a2bbfdd9b69a4e0f676ec3849999d13fd11
norefice-github <nickjoref@gmail.com>
2023-10-11 20:27:31 -0500

    Change app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Ola, Docker fans, from Juvenile!');
+  res.send('Ola, Docker fans, from Juvenile! From dev branch');
 });
 
 app.listen(port, () => {
