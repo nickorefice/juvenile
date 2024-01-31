4b8c439321fc500e00e98cda8f382299ee4f3b3b
norefice-github <nickjoref@gmail.com>
2024-01-31 10:59:12 -0600

    edit app.js to change text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hi friends, from Juvenile!');
+  res.send('Hola amigos, from Juvenile!');
 });
 
 app.listen(port, () => {
